e704b16bc0546f3042930ddee738ad5e74c67639
DavidKim <115195328+david-YJ-Kim@users.noreply.github.com>
2024-02-04 21:08:15 -0500

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+Custom log appender for workflow app
+
+logback document : https://logback.qos.ch/manual/appenders.html#WriteYourOwnAppender
+sample code : https://github.com/qos-ch/logback/blob/master/logback-examples/src/main/java/chapters/appenders/CountingConsoleAppender.java
+
+blog 
+1. https://blog.naver.com/dkdnblack/220503395795
+2. 
---
+++ src/main/java/com/abs/wfs/appender/WfsLogAppender.java
@@ -0,0 +1,64 @@
+package com.abs.wfs.appender;
+
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+
+public class WfsLogAppender extends AppenderBase<ILoggingEvent> {
+
+
+    private String url;
+    private String port;
+    private String vpn;
+    private String password;
+
+    private String username;
+
+    PatternLayoutEncoder encoder;
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getVpn() {
+        return vpn;
+    }
+
+    public void setVpn(String vpn) {
+        this.vpn = vpn;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    protected void append(ILoggingEvent iLoggingEvent) {
+
+    }
+}
